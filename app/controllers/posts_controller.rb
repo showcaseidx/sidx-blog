7c2cd41da1e532813d7cc8285de7acff732ea0d0
Robert D. Cotey II <coteyr@coteyr.net>
2021-07-20 10:54:41 -0400

    Create post on JSON

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,20 @@ class PostsController < ApplicationController
   def index
     render json: Post.all
   end
+
+  def create
+    @post = Post.new
+    if @post.update(allowed_params)
+      render json: @post
+    else
+      render json: @post.errors.to_json
+    end
+  end
+
+  private
+
+  def allowed_params
+    params.require(:post).permit(:title, :body)
+  end
 end
+
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -13,4 +13,35 @@ RSpec.describe "Posts", type: :request do
       expect(response.body).to include(post.to_json)
     end
   end
+
+  describe "POST /posts" do
+    before :each do
+      @post_attrs = FactoryBot.attributes_for(:post)
+      @headers = { "HTTP_ACCEPT" => "application/json" }
+    end
+
+    it 'create a post when posting' do
+      post posts_path, params: { post: @post_attrs }, headers: @headers
+      expect(Post.count).to eq(1)
+    end
+
+    it 'return 200 on success' do
+      post posts_path, params: { post: @post_attrs }, headers: @headers
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns the post attribs on successful post' do
+      post posts_path, params: { post: @post_attrs }, headers: @headers
+      obj = JSON.parse(response.body)
+      expect(obj['id']).to_not eq(nil)
+      expect(obj['title']).to eq(Post.first.title)
+    end
+
+    xit 'returns errors on failed validations' do
+      pending 'There are no current validations'
+    end
+
+
+  end
+
 end
